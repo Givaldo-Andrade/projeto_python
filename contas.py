a6f15302aa1ca92b38fb33b5ebb10b2bc328cb3e
Givaldo Dev <81401949+Givaldo-Andrade@users.noreply.github.com>
2023-11-14 13:54:59 -0300

    Update contas.py

--- contas.py
+++ contas.py
@@ -1,6 +1,13 @@
 import os
 from datetime import datetime, date
 
+# Icaro e Thasio
+
+class Status(object):
+    def __init__(self):
+        self.atrasado = 'Atrasado'
+        self.pendente = 'Pendente'
+        
 class Conta(object):
     def __init__(self, id: int, descricao: str, fornecedor: str, vencimento: str, valor: float, valorTotal: float, data: str, status: str):
         self.id = id
@@ -8,11 +15,12 @@ class Conta(object):
         self.fornecedor = fornecedor
         self.vencimento = vencimento
         self.valor = valor
-        self.valorTotal = valorTotal
+        self.valorTotal_Cont = valorTotal
         self.data = data
         self.status = status
     
 class Contas(object):
+    
     def __init__(self):
         self.list_contas = list()
         self.Total = 0
@@ -23,43 +31,55 @@ class Contas(object):
     
     def imprimir(self):
         os.system('cls')
-        print('''
+        data_hoje = datetime.now().strftime('%d/%m/%Y - %H:%M:%S')
+        status = Status()
+        print(f'''
 --------------------------------------------         
 |  PROGRAMA DE CADASTRO DE CONTAS A PAGAR  |
 --------------------------------------------
+Data/Hora: {data_hoje}
+--------------------------------------------
 
-OPÇÂO - 8
+OPÇÃO - 8
 Ver Transações de Conta
 --------------------------------------------''')
         if len(self.list_contas) == 0:
             print("\nNenhuma conta cadastrada.\n")
         
         else:
+            print('''
+---------------------------------
+|       REGISTRO - CONTAS       |
+--------------------------------------------------------------------------------------------------------------------------------------------''')
             for conta in self.list_contas:
-                print(f'''
-----------------------------------
-|        REGISTRO - CONTA        |
-----------------------------------
-ID: {conta.id}
-Fornecedor: {conta.fornecedor}
-Descrição: {conta.descricao}
-Valor: R${conta.valor:.2f}
-Data Emissao: {conta.data}
-Data Vencimento: {conta.vencimento}
-Status: {conta.status}
-----------------------------------''')
+                vencimento = conta.vencimento
+                d, m, y = [int(i) for i in vencimento.split('/')]
+                vencimento = date(y, m, d)
+                hoje = date.today()
+        
+                if hoje <= vencimento:
+                    conta.status = status.pendente
+                    
+                else:
+                    conta.status = status.atrasado
+                    
+                print(f'''ID: {conta.id} | Descrição: {conta.descricao} | Fornecedor: {conta.fornecedor} | Vencimento: {conta.vencimento} | Valor: R${conta.valor:.2f} | Data: {conta.data} | Status: {conta.status}
+--------------------------------------------------------------------------------------------------------------------------------------------''')
             print(f"Valor Total: R${self.Total:.2f}\n")
             
         input("Pressione ENTER para voltar.")
     
     def remover(self):
         os.system('cls')
-        print('''
+        data_hoje = datetime.now().strftime('%d/%m/%Y - %H:%M:%S')
+        print(f'''
 --------------------------------------------         
 |  PROGRAMA DE CADASTRO DE CONTAS A PAGAR  |
 --------------------------------------------
+Data/Hora: {data_hoje}
+--------------------------------------------
 
-OPÇÂO - 4
+OPÇÃO - 4
 Remover Contas
 --------------------------------------------
 ''')
@@ -93,45 +113,53 @@ Status: {conta.status}
                     conta.id = d + 1
                     
                 print(f'''{item_del}
-Item Deletado com Sucesso.
+Conta Deletada com Sucesso.
 ''')
                 
             except:
-                print("\nID Inválido.")
-            
+                print("\nID Inválido.")    
         
         else:
             print("Nenhum Registro para Deletar.\n")    
         
         input('Pressione ENTER para Voltar.')
     
-    def insert(self):            
+    def insert(self):
+        # fornecedor = fornecedores.Fornecedores()
+        # fornecedor.imprimir()
+                  
         while True:
             os.system('cls')
-            print('''
+            data_hoje = datetime.now().strftime('%d/%m/%Y - %H:%M:%S')
+            status = Status()
+            print(f'''
 --------------------------------------------         
 |  PROGRAMA DE CADASTRO DE CONTAS A PAGAR  |
 --------------------------------------------
+Data/Hora: {data_hoje}
+--------------------------------------------
 
-OPÇÂO - 3
+OPÇÃO - 3
 Cadastrar Contas
 --------------------------------------------''')
+
             n = len(self.list_contas)
             id = n + 1    
             descricao = input("\nInforme a descricao: ")
-            fornecedor = input("Informe o nome do fornecedor: ")
+            fornecedor = input("Informe o fornecedor: ")
             try:
                 d, m, y = [int(i) for i in input("Informe a data de vencimento (DD/MM/AAAA): ").split('/')]
                 vencimento = date(y, m, d)
                 tempo = date.today()
         
-                if tempo > vencimento:
-                    status = 'Atrasado'
+                if tempo <= vencimento:
+                    status = status.pendente
                     
                 else:
-                    status = 'Pendente'
+                    status = status.atrasado
                     
-                vencimento = vencimento.strftime("%d/%m/%y")
+                vencimento = vencimento.strftime("%d/%m/%Y")
+                
             except:
                 input('''\n<ERROR> Informe a data no formato correto :(.
 Pressione ENTER para voltar.''')
@@ -139,13 +167,14 @@ Pressione ENTER para voltar.''')
             
             try:
                 valor = float(input("Informe o valor: R$"))
+                
             except:
                 input('''<ERROR> Informe apenas números no valor :(. 
 Pressione ENTER para voltar.''')
                 break
                 
             self.Total += valor
-            data = datetime.now().strftime('%d/%m/%y - %H:%M:%S')
+            data = datetime.now().strftime('%d/%m/%Y - %H:%M:%S')
             cont = Conta(id=id, descricao=descricao, fornecedor=fornecedor, vencimento=vencimento, valor=valor, valorTotal=self.Total, data=data, status=status) 
             self.add(cont)
             
